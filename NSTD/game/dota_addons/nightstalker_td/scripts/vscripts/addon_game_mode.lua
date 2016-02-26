ed89f4be3104a4421640079de9d6881231d32ba5
Arne <arne.95@hotmail.com>
2016-02-26 11:22:39 +0800

    Reverse commit

--- NSTD/game/dota_addons/nightstalker_td/scripts/vscripts/addon_game_mode.lua
+++ NSTD/game/dota_addons/nightstalker_td/scripts/vscripts/addon_game_mode.lua
@@ -1,8 +1,10 @@
 -- Generated from template
 
-if CAddonTemplateGameMode == nil then
-	CAddonTemplateGameMode = class({})
+if MyGameMode == nil then
+	_G.MyGameMode = class({})
 end
+
+require('gamemode')
 
 function Precache( context )
 	--[[
@@ -16,21 +18,5 @@ end
 
 -- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = CAddonTemplateGameMode()
-	GameRules.AddonTemplate:InitGameMode()
-end
-
-function CAddonTemplateGameMode:InitGameMode()
-	print( "Template addon is loaded." )
-	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
-end
-
--- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
-	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print( "Template addon script is running." )
-	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
-		return nil
-	end
-	return 1
+	MyGameMode:InitGameMode()
 end
